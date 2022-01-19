c5d1b43e6a2a7a3b2accf3506f4407940617a5eb
Zoé <zoepesleuxzp@gmail.com>
2022-01-19 14:26:30 +0100

    classicGame

--- Assets/Scripts/Managers/ClassicGameManager.cs
+++ Assets/Scripts/Managers/ClassicGameManager.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class ClassicGameManager : MonoBehaviour
 {
-    enum Emoji
+    public enum Emoji
     {
-        none,
+        none1,
+        none2,
         angry,
         crying,
         embarassed,
@@ -15,21 +16,9 @@ public class ClassicGameManager : MonoBehaviour
         smiling
     }
 
-    Emoji selected1 = Emoji.none;
-    Emoji selected2 = Emoji.none;
+    public Emoji selected1 = Emoji.none1;
+    public Emoji selected2 = Emoji.none2;
 
-    public bool emoji1A;
-    public bool emoji2A;
-    public bool emoji1B;
-    public bool emoji2B;
-    public bool emoji1C;
-    public bool emoji2C;
-    public bool emoji1D;
-    public bool emoji2D;
-    public bool emoji1E;
-    public bool emoji2E;
-    public bool emoji1F;
-    public bool emoji2F;
 
     public GameObject angry1;
     public GameObject angry2;
@@ -47,55 +36,16 @@ public class ClassicGameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        emoji1A = false;
-        emoji2A = false;
-        emoji1B = false;
-        emoji2B = false;
-        emoji1C = false;
-        emoji2C = false;
-        emoji1D = false;
-        emoji2D = false;
-        emoji1E = false;
-        emoji2E = false;
-        emoji1F = false;
-        emoji2F = false;
-
-
         SpawnEmojis();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (emoji1A & emoji2A)
+        if (selected1 == selected2)
         {
-            Destroy(angry1);
-            Destroy(GameObject.Find("angry2").gameObject);
-        }
-        if (emoji1B & emoji2B)
-        {
-            Destroy(GameObject.Find("crying1").gameObject);
-            Destroy(GameObject.Find("crying2").gameObject);
-        }
-        if (emoji1C & emoji2C)
-        {
-            Destroy(GameObject.Find("embarassed1").gameObject);
-            Destroy(GameObject.Find("embarassed2").gameObject);
-        }
-        if (emoji1D & emoji2D)
-        {
-            Destroy(GameObject.Find("laughing1").gameObject);
-            Destroy(GameObject.Find("laughing2").gameObject);
-        }
-        if (emoji1E & emoji2E)
-        {
-            Destroy(GameObject.Find("scared1").gameObject);
-            Destroy(GameObject.Find("scared2").gameObject);
-        }
-        if (emoji1F & emoji2F)
-        {
-            Destroy(GameObject.Find("smiling1").gameObject);
-            Destroy(GameObject.Find("smiling2").gameObject);
+            Destroy(GameObject.Find(selected1.ToString() + "1").gameObject);
+            Destroy(GameObject.Find(selected1+"2").gameObject);
         }
 
         if (GameObject.FindGameObjectsWithTag("Emoji").Length == 0)
@@ -107,103 +57,131 @@ public class ClassicGameManager : MonoBehaviour
 
     public void ClickOnAngry1()
     {
-        OnClick("angry1", emoji1A, out emoji1A);
-        selected1 = Emoji.angry;
+        OnClick1("angry1", Emoji.angry);
     }
 
     public void ClickOnAngry2()
     {
-        OnClick("angry2", emoji2A, out emoji2A);
-        selected2 = Emoji.angry;
+        OnClick2("angry2", Emoji.angry);
     }
 
     public void ClickOnCrying1()
     {
-        OnClick("crying1", emoji1B, out emoji1B);
-        selected1 = Emoji.crying;
+        OnClick1("crying1", Emoji.crying);
     }
 
     public void ClickOnCrying2()
     {
-        OnClick("crying2", emoji2B, out emoji2B);
-        selected2 = Emoji.crying;
+        
+        OnClick2("crying2", Emoji.crying);
     }
     public void ClickOnEmbarassed1()
     {
-        OnClick("embarassed1", emoji1C, out emoji1C);
+        OnClick1("embarassed1", Emoji.embarassed);
     }
 
     public void ClickOnEmbarassed2()
     {
-        OnClick("embarassed2", emoji2C, out emoji2C);
+        OnClick2("embarassed2", Emoji.embarassed);
     }
     public void ClickOnLaughing1()
     {
-        OnClick("laughing1", emoji1D, out emoji1D);
+        OnClick1("laughing1", Emoji.laughing);
     }
 
     public void ClickOnLaughing2()
     {
-        OnClick("laughing2", emoji2D, out emoji2D);
+        OnClick2("laughing2", Emoji.laughing);
     }
 
     public void ClickOnScared1()
     {
-        OnClick("scared1", emoji1E, out emoji1E);
+        OnClick1("scared1", Emoji.scared);
     }
 
     public void ClickOnScared2()
     {
-        OnClick("scared2", emoji2E, out emoji2E);
+        OnClick2("scared2", Emoji.scared);
     }
     public void ClickOnSmiling1()
     {
-        OnClick("smiling2", emoji1F, out emoji1F);
+        OnClick1("smiling2", Emoji.smiling);
     }
 
     public void ClickOnSmiling2()
     {
-        OnClick("smiling2", emoji2F, out emoji2F);
+        OnClick2("smiling2", Emoji.smiling);
     }
 
-    void OnClick(string gameObjectName,in bool isEmojiActive,  out bool emojiActive)
+    void OnClick1(string gameObjectName, Emoji emojiName)
     {
-        if (!isEmojiActive)
+        if (selected1.CompareTo(emojiName) == 0)
         {
-            emojiActive = true;
-            GameObject.Find(gameObjectName).transform.localScale = new Vector3(75f, 75f, 75f);
+            selected1 = Emoji.none1;
+            GameObject.Find(gameObjectName).transform.localScale = new Vector3(50f, 50f, 50f);
+
         }
         else
         {
-            emojiActive = false;
+            if (selected1.CompareTo(Emoji.none1) != 0)
+            {
+                GameObject.Find(selected1.ToString() + "1").transform.localScale = new Vector3(50f, 50f, 50f);
+            }
+            selected1 = emojiName;
+            GameObject.Find(gameObjectName).transform.localScale = new Vector3(75f, 75f, 75f);
+        }
+    }
+
+    void OnClick2(string gameObjectName, Emoji emojiName)
+    {
+        if (selected2.CompareTo(emojiName)==0)
+        {
             GameObject.Find(gameObjectName).transform.localScale = new Vector3(50f, 50f, 50f);
+            selected2 = Emoji.none2;
+        }
+        else
+        {
+            if(selected2.CompareTo(Emoji.none2) != 0)
+            {
+                GameObject.Find(selected2.ToString() + "2").transform.localScale = new Vector3(50f, 50f, 50f);
+            }
+            
+            selected2 = emojiName;
+            GameObject.Find(gameObjectName).transform.localScale = new Vector3(75f, 75f, 75f);
+
         }
     }
 
     public void SpawnEmojis()
     {
-        crying1.SetActive(true);
-        crying2.SetActive(true);
         angry1.SetActive(true);
         angry2.SetActive(true);
+        crying1.SetActive(true);
+        crying2.SetActive(true);
+        embarassed1.SetActive(true);
+        embarassed2.SetActive(true);
         laughing1.SetActive(true);
         laughing2.SetActive(true);
-        /*
-        Instantiate(Resources.Load("Models/angry"), new Vector3(0.15f, -0.239999f, -1.91f), Quaternion.identity);
-        GameObject.Find("angry(Clone)").name = "angry1";
+        laughing1.SetActive(true);
+        scared1.SetActive(true);
+        smiling1.SetActive(true);
+        smiling2.SetActive(true);
+    /*
+    Instantiate(Resources.Load("Models/angry"), new Vector3(0.15f, -0.239999f, -1.91f), Quaternion.identity);
+    GameObject.Find("angry(Clone)").name = "angry1";
 
-        Instantiate(Resources.Load("Models/angry"), new Vector3(-0.2f, 7.9f, 1.5f), Quaternion.identity);
-        GameObject.Find("angry(Clone)").name = "angry2";
-        /*
-        if (PhotonManager.instance.NumberOfImages == 4)
-        {
+    Instantiate(Resources.Load("Models/angry"), new Vector3(-0.2f, 7.9f, 1.5f), Quaternion.identity);
+    GameObject.Find("angry(Clone)").name = "angry2";
+    /*
+    if (PhotonManager.instance.NumberOfImages == 4)
+    {
 
-        } else if (PhotonManager.instance.NumberOfImages == 5)
-        {
+    } else if (PhotonManager.instance.NumberOfImages == 5)
+    {
 
-        }*/
+    }*/
 
-    }
+}
 
      void victory()
     {
