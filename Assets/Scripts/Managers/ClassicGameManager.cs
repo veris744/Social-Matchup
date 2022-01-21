a70ac99b96107a9ccda229d2cc394617e97885bb
Zoé <zoepesleuxzp@gmail.com>
2022-01-21 10:39:03 +0100

    Revert "try"
    This reverts commit 87d49e2b2b63dc66600d6ae7b2731fc41b5e9490.

--- Assets/Scripts/Managers/ClassicGameManager.cs
+++ Assets/Scripts/Managers/ClassicGameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class ClassicGameManager : GameManager
 {
-    public enum EmojiEnum
+    public enum Emoji
     {
         none1,
         none2,
@@ -17,15 +17,14 @@ public class ClassicGameManager : GameManager
         smiling
     }
 
-    public EmojiEnum selected1 = EmojiEnum.none1;
-    public EmojiEnum selected2 = EmojiEnum.none2;
+    public Emoji selected1 = Emoji.none1;
+    public Emoji selected2 = Emoji.none2;
 
     Vector3 baseEmojiPosition1 = new Vector3(0, 3, 1.1f);
     Vector3 baseEmojiPosition2 = new Vector3(0, 3, -1.1f);
 
-    public Emoji angry1;
-    public Emoji angry2;
-    /*
+    public GameObject angry1;
+    public GameObject angry2;
     public GameObject crying1;
     public GameObject crying2;
     public GameObject embarassed1;
@@ -36,7 +35,7 @@ public class ClassicGameManager : GameManager
     public GameObject scared2;
     public GameObject smiling1;
     public GameObject smiling2;
-    */
+
     int init = 0;
 
 
@@ -85,73 +84,73 @@ public class ClassicGameManager : GameManager
 
     public void ClickOnAngry1()
     {
-        OnClick1("angry1", EmojiEnum.angry);
+        OnClick1("angry1", Emoji.angry);
     }
 
     public void ClickOnAngry2()
     {
-        OnClick2("angry2", EmojiEnum.angry);
+        OnClick2("angry2", Emoji.angry);
     }
-    /*
+
     public void ClickOnCrying1()
     {
-        OnClick1("crying1", EmojiEnum.crying);
+        OnClick1("crying1", Emoji.crying);
     }
 
     public void ClickOnCrying2()
     {
         
-        OnClick2("crying2", EmojiEnum.crying);
+        OnClick2("crying2", Emoji.crying);
     }
     public void ClickOnEmbarassed1()
     {
-        OnClick1("embarassed1", EmojiEnum.embarassed);
+        OnClick1("embarassed1", Emoji.embarassed);
     }
 
     public void ClickOnEmbarassed2()
     {
-        OnClick2("embarassed2", EmojiEnum.embarassed);
+        OnClick2("embarassed2", Emoji.embarassed);
     }
     public void ClickOnLaughing1()
     {
-        OnClick1("laughing1", EmojiEnum.laughing);
+        OnClick1("laughing1", Emoji.laughing);
     }
 
     public void ClickOnLaughing2()
     {
-        OnClick2("laughing2", EmojiEnum.laughing);
+        OnClick2("laughing2", Emoji.laughing);
     }
 
     public void ClickOnScared1()
     {
-        OnClick1("scared1", EmojiEnum.scared);
+        OnClick1("scared1", Emoji.scared);
     }
 
     public void ClickOnScared2()
     {
-        OnClick2("scared2", EmojiEnum.scared);
+        OnClick2("scared2", Emoji.scared);
     }
     public void ClickOnSmiling1()
     {
-        OnClick1("smiling2", EmojiEnum.smiling);
+        OnClick1("smiling2", Emoji.smiling);
     }
 
     public void ClickOnSmiling2()
     {
-        OnClick2("smiling2", EmojiEnum.smiling);
+        OnClick2("smiling2", Emoji.smiling);
     }
-    */
-    void OnClick1(string gameObjectName, EmojiEnum emojiName)
+
+    void OnClick1(string gameObjectName, Emoji emojiName)
     {
         if (selected1.CompareTo(emojiName) == 0)
         {
-            selected1 = EmojiEnum.none1;
+            selected1 = Emoji.none1;
             GameObject.Find(gameObjectName).transform.localScale = new Vector3(50f, 50f, 50f);
 
         }
         else
         {
-            if (selected1.CompareTo(EmojiEnum.none1) != 0)
+            if (selected1.CompareTo(Emoji.none1) != 0)
             {
                 GameObject.Find(selected1.ToString() + "1").transform.localScale = new Vector3(50f, 50f, 50f);
             }
@@ -160,16 +159,16 @@ public class ClassicGameManager : GameManager
         }
     }
 
-    void OnClick2(string gameObjectName, EmojiEnum emojiName)
+    void OnClick2(string gameObjectName, Emoji emojiName)
     {
         if (selected2.CompareTo(emojiName)==0)
         {
             GameObject.Find(gameObjectName).transform.localScale = new Vector3(50f, 50f, 50f);
-            selected2 = EmojiEnum.none2;
+            selected2 = Emoji.none2;
         }
         else
         {
-            if(selected2.CompareTo(EmojiEnum.none2) != 0)
+            if(selected2.CompareTo(Emoji.none2) != 0)
             {
                 GameObject.Find(selected2.ToString() + "2").transform.localScale = new Vector3(50f, 50f, 50f);
             }
@@ -182,9 +181,8 @@ public class ClassicGameManager : GameManager
 
     public void SpawnEmojis()
     {
-        angry1.gameObj = PhotonNetwork.Instantiate("Models/Prefab/Angry", baseEmojiPosition1, Quaternion.identity, 0);
-        angry2.gameObj = PhotonNetwork.Instantiate("Models/Prefab/Angry", baseEmojiPosition2, Quaternion.identity, 0);
-
+        angry1 = PhotonNetwork.Instantiate("Models/Prefab/Angry", baseEmojiPosition1, Quaternion.identity, 0);
+        angry2 = PhotonNetwork.Instantiate("Models/Prefab/Angry", baseEmojiPosition2, Quaternion.identity, 0);
         /*
         crying1 = PhotonNetwork.Instantiate("Models/Prefab/Crying", baseEmojiPosition1, Quaternion.identity, 0);
         crying2 = PhotonNetwork.Instantiate("Models/Prefab/Crying", baseEmojiPosition1, Quaternion.identity, 0);
