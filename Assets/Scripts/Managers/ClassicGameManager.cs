94ed4bbe786e462a8c6f0b025e880807410b3807
Zoé <zoepesleuxzp@gmail.com>
2022-01-21 12:12:24 +0100

    clean, select still don't work

--- Assets/Scripts/Managers/ClassicGameManager.cs
+++ Assets/Scripts/Managers/ClassicGameManager.cs
@@ -29,18 +29,6 @@ public class ClassicGameManager : GameManager
     private GameObject angryButton1;
     private GameObject angryButton2;
 
-    /*
-public GameObject crying1;
-public GameObject crying2;
-public GameObject embarassed1;
-public GameObject embarassed2;
-public GameObject laughing1;
-public GameObject laughing2;
-public GameObject scared1;
-public GameObject scared2;
-public GameObject smiling1;
-public GameObject smiling2;
-*/
     int init = 0;
 
 
@@ -58,7 +46,6 @@ public GameObject smiling2;
             init++;
             if (PhotonNetwork.IsMasterClient)
             {
-                Debug.Log("master");
                 SpawnEmojis();
             }
         }
@@ -66,13 +53,12 @@ public GameObject smiling2;
 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("getMouseDown");
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100))
             {
-                Debug.Log(hit.transform.gameObject.name);
+                Debug.Log("Object clicked: " + hit.transform.gameObject.name);
             }
         }
         if (selected1 == selected2)
@@ -99,55 +85,7 @@ public GameObject smiling2;
         Debug.Log("ClickOnAngry2");
         OnClick2("angry2", EmojiEnum.angry);
     }
-    /*
-    public void ClickOnCrying1()
-    {
-        OnClick1("crying1", EmojiEnum.crying);
-    }
-
-    public void ClickOnCrying2()
-    {
-        
-        OnClick2("crying2", EmojiEnum.crying);
-    }
-    public void ClickOnEmbarassed1()
-    {
-        OnClick1("embarassed1", EmojiEnum.embarassed);
-    }
-
-    public void ClickOnEmbarassed2()
-    {
-        OnClick2("embarassed2", EmojiEnum.embarassed);
-    }
-    public void ClickOnLaughing1()
-    {
-        OnClick1("laughing1", EmojiEnum.laughing);
-    }
-
-    public void ClickOnLaughing2()
-    {
-        OnClick2("laughing2", EmojiEnum.laughing);
-    }
-
-    public void ClickOnScared1()
-    {
-        OnClick1("scared1", EmojiEnum.scared);
-    }
-
-    public void ClickOnScared2()
-    {
-        OnClick2("scared2", EmojiEnum.scared);
-    }
-    public void ClickOnSmiling1()
-    {
-        OnClick1("smiling2", EmojiEnum.smiling);
-    }
-
-    public void ClickOnSmiling2()
-    {
-        OnClick2("smiling2", EmojiEnum.smiling);
-    }
-    */
+    
     void OnClick1(string gameObjectName, EmojiEnum emojiName)
     {
         if (selected1.CompareTo(emojiName) == 0)
@@ -199,27 +137,15 @@ public GameObject smiling2;
         EventTrigger trigger = angryButton1.GetComponent<EventTrigger>();
         EventTrigger.Entry entry = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerClick;
-        entry.callback.AddListener((eventData) => { Debug.Log("event trigger angry1"); });
+        entry.callback.AddListener((eventData) => { ClickOnAngry1(); });
         trigger.triggers.Add(entry);
 
         angryButton2.AddComponent(typeof(EventTrigger));
         EventTrigger trigger2 = angryButton1.GetComponent<EventTrigger>();
         EventTrigger.Entry entry2 = new EventTrigger.Entry();
         entry.eventID = EventTriggerType.PointerClick;
-        entry.callback.AddListener((eventData) => { Debug.Log("event trigger angry1"); });
+        entry.callback.AddListener((eventData) => { ClickOnAngry2(); });
         trigger.triggers.Add(entry);
-        /*
-        crying1 = PhotonNetwork.Instantiate("Models/Prefab/Crying", baseEmojiPosition1, Quaternion.identity, 0);
-        crying2 = PhotonNetwork.Instantiate("Models/Prefab/Crying", baseEmojiPosition1, Quaternion.identity, 0);
-        embarassed1 = PhotonNetwork.Instantiate("Models/Prefab/Embarassed", baseEmojiPosition1, Quaternion.identity, 0);
-        embarassed2 = PhotonNetwork.Instantiate("Models/Prefab/Embarassed", baseEmojiPosition1, Quaternion.identity, 0);
-        laughing1 = PhotonNetwork.Instantiate("Models/Prefab/Laughing", baseEmojiPosition1, Quaternion.identity, 0);
-        laughing2 = PhotonNetwork.Instantiate("Models/Prefab/Laughing", baseEmojiPosition1, Quaternion.identity, 0);
-        scared1 = PhotonNetwork.Instantiate("Models/Prefab/Scared", baseEmojiPosition1, Quaternion.identity, 0);
-        scared2 = PhotonNetwork.Instantiate("Models/Prefab/Scared", baseEmojiPosition1, Quaternion.identity, 0);
-        smiling1 = PhotonNetwork.Instantiate("Models/Prefab/Smiling", baseEmojiPosition1, Quaternion.identity, 0);
-        smiling2 = PhotonNetwork.Instantiate("Models/Prefab/Smiling", baseEmojiPosition1, Quaternion.identity, 0);
-        */
 
     }
 
