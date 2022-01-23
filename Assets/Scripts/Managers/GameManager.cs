d82abfd6d4de21560f295749cc26beba203a5597
Isabel <isadede@alum.us.es>
2022-01-23 20:49:38 +0100

    fixes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -33,14 +33,14 @@ public class GameManager : MonoBehaviour
                 foreach (GameObject player in players)
                 {
                     Debug.Log("Player: " + player.GetPhotonView().IsMine);
-                    player.transform.Find("Camera Offset").gameObject.SetActive(false);
+                    player.transform.Find("Camera Offset").Find("Main Camera").gameObject.SetActive(false);
 
                     if (player.GetPhotonView().IsMine)
                     {
                         Debug.Log("Player Mine: " + player.GetPhotonView().ViewID);
                         thisPlayer = player.gameObject;
 
-                        player.transform.Find("Camera Offset").gameObject.SetActive(true);
+                        player.transform.Find("Camera Offset").Find("Main Camera").gameObject.SetActive(true);
                         CameraController cameraController = player.transform.Find("Camera Offset").Find("Main Camera").gameObject.GetComponent<CameraController>();
                         cameraController.enabled = true;
                         cameraController.SetTarget(player.transform);
@@ -54,14 +54,14 @@ public class GameManager : MonoBehaviour
         if (GameObject.FindGameObjectsWithTag("Helper").Length == 1)
         {
             helper = GameObject.FindGameObjectsWithTag("Helper")[0];
-            helper.transform.Find("Camera Offset").gameObject.SetActive(false);
+            helper.transform.Find("Camera Offset").Find("Main Camera").gameObject.SetActive(false);
             if (helper.GetPhotonView().IsMine)
             {
                 thisPlayer = PhotonManager.instance.Helper;
                 Debug.Log("Player Mine: " + helper.GetInstanceID());
                 thisPlayer = helper.gameObject;
 
-                helper.transform.Find("Camera Offset").gameObject.SetActive(true);
+                helper.transform.Find("Camera Offset").Find("Main Camera").gameObject.SetActive(true);
                 CameraController cameraController = helper.transform.Find("Camera Offset").Find("Main Camera").gameObject.GetComponent<CameraController>();
                 cameraController.enabled = true;
                 cameraController.SetTarget(helper.transform);
@@ -80,39 +80,4 @@ public class GameManager : MonoBehaviour
     }
 
 
-    [PunRPC]
-    public void StartVictoryAnimations()
-    {
-        StartCoroutine(OnVictory());
-    }
-    /*
-    [PunRPC]
-    public void PvpHurraySound(int teamIndex)
-    {
-        if (GetTeam(thisPlayer.gameObject.GetPhotonView().Owner.ActorNumber) == teamIndex)
-            AudioManager.instance.PlayHurraySound();
-    }*/
-
-    protected IEnumerator OnVictory()
-    {
-        yield return new WaitForSeconds(1);
-        AudioManager.instance.PlayHurraySound();
-        yield return new WaitForSeconds(3);
-        AudioManager.instance.StopMusic();
-        yield return new WaitForSeconds(2);
-        AudioManager.instance.PlayVictorySound();
-
-        yield return new WaitForSeconds(6);
-        SceneManager.LoadScene("MainMenu");
-
-        PhotonNetwork.LeaveRoom();
-    }
-
-    /*
-    public int GetTeam(int playerId)
-    {
-        if (playerId >= 1 && playerId <= 2) return 1;
-        if (playerId >= 3 && playerId <= 4) return 2;
-        return 0;
-    }*/
 }
